a225e7e8555e4580c764081902834c7cda7c7201
vadimmbld <67420180+vadimmbld@users.noreply.github.com>
2021-05-09 09:27:46 +0300

    Add files via upload

---
+++ word3D.js
@@ -0,0 +1,92 @@
+let body = document.querySelector('body');
+let word3d = new function(){
+    this.mchars = 0;
+    this.word = [];
+    this.wordInd = 0;
+    this.spinBox =(elem,maxwords)=>{
+        let sides = Array.prototype.slice.call(elem.side);
+    
+    
+        for(let i=0;i<sides.length;i++){
+            if(sides[i].dataset.index == 1){
+                sides[i].dataset.index = maxwords+1;
+                sides[i].classList.add('down');
+                sides[i].classList.remove('current');
+                setTimeout(()=>{
+                    sides[i].classList.remove('down');
+                },900)
+                break;
+            }
+            
+        }
+        for(let i=0;i<sides.length;i++){
+            sides[i].dataset.index--;
+            if(sides[i].dataset.index == 1){
+                sides[i].classList.add('current');
+            }
+        }
+    }
+
+    this.nextWord = ()=>{
+        let p = [];
+        this.wordInd++;
+        if(this.wordInd>this.mchars){
+            this.wordInd = 0;
+        }
+        document.querySelector('title').innerText = `${this.word[this.wordInd]}`;
+        this.box.forEach((el,i)=>{
+            
+            p[i] = new Promise((resolve)=>{
+                setTimeout(()=>{
+                    this.spinBox(el,this.box[i].side.length);
+                    resolve();
+                },100*i)
+                
+            });
+        });
+        
+        Promise.all(p).then(()=>{
+            
+            setTimeout(()=>{
+                this.nextWord()
+                
+            },1000)
+        })
+    }
+
+    this.init = (elem,words)=>{
+        this.mchars = words[0].length;
+        this.mainElem = document.createElement('main');
+        this.mainElem.classList.add('word3d');
+        this.box = [];
+        this.word = words;
+        this.wordInd = 0;
+        document.querySelector('title').innerText = `${this.word[this.wordInd]}`;
+        elem.appendChild(this.mainElem);
+        for(let i=0;i<this.mchars;i++){
+            
+            this.box.push(document.createElement('ul'));
+            this.box[i].classList.add('box');
+            this.box[i].side = [];
+            this.mainElem.appendChild(this.box[i])
+            for(let j=0;j<words.length;j++){
+                
+                this.box[i].side.push(document.createElement('li'));
+                
+                this.box[i].side[j].classList.add('side');
+                if(j==0){
+                    this.box[i].side[j].classList.add('current');
+                }
+                this.box[i].side[j].dataset.index = j+1;
+                this.box[i].side[j].innerHTML = words[j][i];
+                this.box[i].appendChild(this.box[i].side[j]);
+                
+            }
+        }
+    }
+}
+
+word3d.init(body,["ЕБАТЬ","ПИЗДА","НАХУЙ","ЧТОЗА","ХУИТА","БЛЯТЬ"]);
+setTimeout(()=>{
+    word3d.nextWord();
+},1000);
